d792c182c9ba014651182e0a29ab78d6eeb7a7c9
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2023-10-07 08:18:00 +0530

    Update linked_lists_7.cpp

--- linked_lists_7.cpp
+++ linked_lists_7.cpp
@@ -1,5 +1,3 @@
-//deletion in double linked lists (two way linked lists) 
-
 #include <iostream>
 using namespace std;
 
@@ -7,16 +5,18 @@ class Node
 {
     public:
     int data;
-    Node* previous;
     Node* next;
+    Node* previous;
 
+    
     //constructor
-    Node(int d)
+    Node (int data)
     {
-        this->data = d;
-        this->previous = NULL;
+        this->data = data;
         this->next = NULL;
+        this->previous = NULL;
     }
+
 
     //destructor
     ~Node()
@@ -33,39 +33,15 @@ class Node
     }
 };
 
-void print(Node* head)
-{
-    Node* temp = head;
-    while(temp!=NULL)
-    {
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }
-    cout<<endl;
-}
-
-int getLength(Node* head)
-{
-    int length=0;
-    Node* temp = head;
-    while(temp!=NULL)
-    {
-        length++;
-        temp = temp->next;
-    }
-    return length;
-}
-
-
-void insertAtHead(Node* &head,int d)
+void insertionAtHead(Node* &head, int d)
 {
     Node* temp = new Node(d);
     temp->next = head;
-    head->previous = temp; //this is the new step we have added (i have added on my own hehe), which makes this a linked list 
+    head->previous = temp;
     head = temp;
 }
 
-void insertAtTail(Node* &tail,int d)
+void inserionAtTail(Node* &tail, int d)
 {
     Node* temp = new Node(d);
     tail->next = temp;
@@ -73,33 +49,50 @@ void insertAtTail(Node* &tail,int d)
     tail = temp;
 }
 
-void insertAtPosition(Node* &head, int position, int d)
+void insertionInMiddle(Node* &head, int position, int d)
 {
-    if (position==1)
-    {
-        insertAtHead(head,d);
-        return;
-    }
-    else
-    {
-        Node* temp = head;
-        int cnt = 1;
+    Node* temp = head;
+    Node* insert = new Node(d);
+    int cnt = 1;
 
-        while(cnt<position-1)
-        {
-            temp = temp->next;
-            cnt++;
-        }
-        Node* insert = new Node(d);
-        /*1*/ insert->next = temp->next;
-        temp->next->previous = insert;    
-        /*2*/ temp->next = insert; //its VERY IMPORTANT to keep the steps in proper sequence otherwise entire function can give a wrong outputor even an error;
-        insert->previous = temp;
-
+    while(cnt<position-1)
+    {
+        temp = temp->next;
+        cnt++;
     }
+    insert->next = temp->next;
+    temp ->next->previous = insert;
+    temp->next = insert;
+    insert->previous = temp;
 }
 
-void del(Node* &head,Node* &tail,int position)
+void print(Node* &head)
+{
+    Node* temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
+    cout<<endl;
+}
+
+void getlength(Node* &head)
+{
+    Node* temp = head;
+    int ans = 0;
+   
+
+    while(temp!=NULL)
+    {
+        temp = temp->next;
+        ans+=1;
+    }
+    cout<<ans<<endl;
+}
+
+void del(Node* &head, int position,Node* &tail)
 {
     if(position==1)
     {
@@ -109,14 +102,16 @@ void del(Node* &head,Node* &tail,int position)
         temp->next=NULL;
         delete temp;
     }
+    
 
-
-    else{
+    
+    else
+    {
     Node* curr = head;
     Node* prev = NULL;
     int cnt = 1;
 
-    while(cnt<position)
+    while(cnt<position-1)
     {
         prev = curr;
         curr = curr->next;
@@ -125,50 +120,33 @@ void del(Node* &head,Node* &tail,int position)
 
     curr->next->previous = prev;
     prev->next = curr->next;
-    if(curr==tail)
+    if(curr == tail)
     {
-        tail=prev;
+        tail = prev;
     }
-    else
-    {
     curr->next = NULL;
-    curr->previous = NULL;
     delete curr;
     }
 }
-    
 
 
 int main()
 {
-    Node* node1 = new Node(4);
+    Node* node1 = new Node(32);
     Node* head = node1;
     Node* tail = node1;
-
-    int len = getLength(head);
+    insertionAtHead(head,21);
+    inserionAtTail(tail,100);
     print(head);
-    insertAtHead(head,12);
-    cout<<"length of your linked list is: "<<len<<endl;
+    insertionInMiddle(head,2,69);
     print(head);
-
-    insertAtHead(head,14);
-    print(head);
-    
-    insertAtHead(head,140);
-    print(head);
-
-    insertAtHead(head,111);
-    print(head);
-
-    insertAtTail(tail,69);
-    print(head);
-
-    // del(head,1);
-    // print(head);
-
-    del(head,6);
-    print(head);
+    getlength(head);
+    del(head,4,tail);
     cout<<tail->data<<endl;
+    print(head);
 
 
 }
+
+
+
