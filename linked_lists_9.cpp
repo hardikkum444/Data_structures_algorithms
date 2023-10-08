249408cfd1d7f7a6576e90cb0c6c4d200b5e8019
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2023-10-08 16:47:01 +0530

    complete code of linked list reversal -1

--- linked_lists_9.cpp
+++ linked_lists_9.cpp
@@ -1,50 +1,37 @@
-//reverse a linked list
+//reversal of linked list using 
+//1)iterative method
+//2)recursion method-1
 
 #include <iostream>
 using namespace std;
 
 class Node
-{   
-    //we will create class for a single linked lists as, we are
-    //performing reversing on single linked list
+{
     public:
     int data;
     Node* next;
 
+    //constructor
     Node(int data)
     {
         this->data = data;
         this->next = next;
     }
+
+    //deconstructor
+    ~Node()
+    {
+        int value = this->data;
+        
+        //freeing the memory
+        if(this->next!=NULL)
+        {
+            this->next = NULL;
+            delete next;
+        }
+        cout<<"memory is free for data " <<value<<endl;
+    }
 };
-
-Node* reverse(Node* &head)
-{
-    
-if(head->next == NULL)
-    {
-        return head;
-    }
-
-    if(head == nullptr)
-    {
-        return head;     
-    }
-    
-    Node* prev = NULL;
-    Node *curr = head;
-    Node* forward = NULL;
-
-    while(curr!=NULL)
-    {
-        forward = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = forward;
-    }
-
-    return prev;
-}
 
 void insertAtHead(Node* &head,int d)
 {
@@ -53,40 +40,142 @@ void insertAtHead(Node* &head,int d)
     head = temp;
 }
 
+void insertAtTail(Node* &tail,int d)
+{
+    Node* temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void middle(Node* &head, int position, int d)
+{
+    Node* temp = head;
+    int count = 1;
+
+    while(count < position-1)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    Node* insert = new Node(d);
+    insert->next = temp->next;
+    temp->next = insert;
+}
+
+void del(Node* &head, int position)
+{
+    Node* curr = head;
+    Node* prev = NULL;
+    int count = 1;
+
+    while(count<position)
+    {
+        prev = curr;
+        curr = curr->next;
+        count++;
+    }
+
+    prev->next = curr->next;
+    curr->next = NULL;
+    delete curr;
+}
+
+
+
 void print(Node* &head)
 {
-    Node* temp  = head;
+    Node* temp = head;
+    int count = 1;
 
-    while(temp!=NULL)
+    while(temp!=nullptr)
     {
         cout<<temp->data<<" ";
         temp = temp->next;
+        count++;
     }
     cout<<endl;
 }
 
+void reverse(Node* &head, Node* curr, Node* prev)
+{
+    //base case
+    if(curr == NULL)
+    {
+        head = prev;
+        return;
+    }
+
+    Node* forward = curr->next;
+    reverse(head, forward, curr);
+    curr->next = prev;
+}
+    
+Node* reverseNode(Node* &head)
+{
+    Node* curr = head;
+    Node* prev = NULL;
+    reverse(head,curr,prev);
+    return head;
+}
+
+// Node* reverse(Node* &head)
+// {
+//     Node* curr = head;
+//     Node* prev = NULL;
+//     Node* next = NULL;
+
+//     while(curr!=nullptr)
+//     {
+//         next = curr->next;
+//         curr->next = prev;
+//         prev = curr;
+//         curr = next;
+//     }
+//     return prev;    
+// }
+
+//time complexity of this method of reversing a linked list 
+//is O(n), since we are using a loop
+//space complexity of this is O(1) as we have taken constant
+//space
+
 int main()
 {
-    Node* node1 = new Node(23);
+    Node* node1 = new Node(3);
     Node* head = node1;
-    insertAtHead(head,33);
-    insertAtHead(head,66);
+    Node* tail = node1;
+    
+    insertAtHead(head,4);
+    insertAtTail(tail,44);
     print(head);
 
-    head = reverse(head);
+    middle(head,3,69);
     print(head);
 
+    del(head,2); //del 3
+    print(head);
+
+    head = reverseNode(head);
+    print(head);    
 }
-        
 
-//so techinically what we have done is, that we have created a node type
-//function which returns a node as the answer,
-//and we have then made a while loop, which essentially goes and 
-//traversing through all the elements, reversing their link and then 
-//at the end of the function, the value of prev is returned, as that is 
-//essentially our new head
 
-//then when we use the print function, the new value of head is passed
-//and sice the entire linked list has been reversed, 
 
-//the reversed linked list is then printed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
