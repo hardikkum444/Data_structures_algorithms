c5dd9cbe372cc502b6f5d1df6737b51bbdc51392
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2023-10-14 22:53:49 +0530

    slightly more optimized method

--- recursion_8.cpp
+++ recursion_8.cpp
@@ -23,3 +23,29 @@ int main()
     
     cout<<str<<endl;
 }
+
+//slightly more optmized method
+
+// #include <iostream>
+// using namespace std;
+
+
+// void reverse(string &str, int i)
+// {
+//     int j = str.length()-i-1;
+//     if(i>j)
+//     {
+//         return;
+//     }
+//     swap(str[i++],str[(j)--]);
+//     reverse(str,i);
+// }
+
+// int main()
+// {
+//     string str = "hardik";
+//     int size = str.size();
+//     reverse(str,0);
+    
+//     cout<<str<<endl;
+// }
