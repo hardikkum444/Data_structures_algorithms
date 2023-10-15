1d868eda6ce0e71a6ee3e37b9ef8b0f9ccdacda1
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2023-10-15 12:38:39 +0530

    syntax for a queue without using STL

---
+++ queue_2.cpp
@@ -0,0 +1,167 @@
+//basic syntax for development of a queue without using STL
+
+
+//front will always be pointing to the element
+//rear will always be one step ahead of the element
+
+
+#include <iostream>
+using namespace std;
+
+class queue
+{
+    public:
+    int* arr;
+    int size;
+    int front;
+    int rear;
+    
+    queue(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        front = 0;
+        rear = 0;
+    }
+    
+    void push(int d)
+    {
+        if(rear == size)
+        {
+            cout<<"queue is empty"<<endl;
+        }
+        else
+        {
+            arr[rear] = d;
+            rear++;
+        }
+    }
+    
+    int pop()
+    {
+        int ans;
+        if(front == rear)
+        {
+            cout<<"queue is empty, nothing to pop"<<endl;
+            return -1;
+        }
+        else
+        {
+            ans = arr[front];
+            arr[front] = -1;
+            front++;
+            if(front == rear)
+            {
+                front = 0;
+                rear = 0;
+            }
+        }
+        return ans;
+    }
+    
+    int frontElement()
+    {
+        int ans;
+        if(front == rear)
+        {
+            cout<<"queue is empty, nothing to display"<<endl;
+            return -1;
+        }
+        else
+        {
+            ans = arr[front];
+        }
+        return ans;
+    }
+    
+    int backElement()
+    {
+        int ans;
+        if(front == rear)
+        {
+            cout<<"queue is empty, nothing to display"<<endl;
+            return -1;
+        }
+        else
+        {
+            ans = arr[rear-1];
+        }
+        return ans;
+    }
+    
+    bool isEmpty()
+    {
+        if(rear==front)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    
+    
+    
+};
+
+int main()
+{
+    queue q(5);
+    
+    q.push(9);
+    q.push(44);
+    q.push(69);
+    
+    
+    cout<<"the element present at first is: "<<q.frontElement()<<endl;
+    cout<<"the element at the start being popped is: "<<q.pop()<<endl;
+    cout<<"the element present at first is: "<<q.frontElement()<<endl;
+    cout<<"the element present at back is: "<<q.backElement()<<endl;
+    
+    bool empty = q.isEmpty();
+    if(empty)
+    {
+        cout<<"your queue is empty"<<endl;
+    }
+    else
+    {
+        cout<<"your queue is not empty"<<endl;
+    }
+    
+    q.pop();
+    q.pop();
+    //q.pop();
+    
+    //bool empty = q.isEmpty();
+    empty = q.isEmpty();
+    if(empty)
+    {
+        cout<<"your queue is empty"<<endl;
+    }
+    else
+    {
+        cout<<"your queue is not empty"<<endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
