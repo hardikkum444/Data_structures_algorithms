9538ddda00ff1977499640ac15f2739ceff3371f
hardikkum444 <hardikkumawat444@gmail.com>
2023-11-03 22:58:23 +0530

    some theory

---
+++ heap_sort_7.cpp
@@ -0,0 +1,23 @@
+//short notes on some tree concepts
+
+//full binary binary tree
+//binary tree with maximum number of nodes is a full binary tree
+//it can have at the max of 2^(h+1)-1 nodes
+
+//complete binary tree
+//in a complete binary tree, we start filling from the left side of the tree
+//and basically when we fill an array using that analogy, no spaces are left
+
+//heap is always a complete binary tree
+//max heap is a binary tree in which the parent node is always greater than child node
+
+
+//height of a complete binary tree is log n
+
+
+//now for insertion in a maxheap, the time taken for insertion in a heap is 
+//minimum of O(1) [if the element inserted is very small, that it doesn't need and swaping with the parent]
+//maximum time required is O(log n) [if the element is worthy of becoming the root of the tree]
+
+
+
