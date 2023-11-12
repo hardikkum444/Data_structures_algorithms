dcd2c799f798db8c6f599aaa783b23f2ae328da3
hardikkum444 <hardikkumawat444@gmail.com>
2023-11-12 17:59:35 +0530

    basic insertion sort rev

---
+++ arrays_rev_1.cpp
@@ -0,0 +1,36 @@
+//basic insertion sort rev 
+
+//lets quickly revise insertion sort
+
+#include <iostream>
+using namespace std;
+
+void search(int arr[],int key,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==key)
+        {
+            cout<<"your number has been found at the "<<i<<" index"<<endl;
+            exit(0);
+        }
+    }
+    cout<<"your number has not been found"<<endl;
+}
+
+int main()
+{
+    cout<<"enter size"<<endl;
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"what would you like to search for: "<<endl;
+    int key;
+    cin>>key;
+    search(arr,key,n);
+}
