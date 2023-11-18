45f03f9c9504e986731336d0b0830f8bf69eaef1
hardikkum444 <hardikkumawat444@gmail.com>
2023-11-18 21:41:03 +0530

    priority queue algo using arrays

---
+++ rev_101/priority_queue_arr.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int value[100];
+    int priority[100];
+    int ans[100];
+
+    int cnt=0;
+    int i=0;
+
+    bool choice = true;
+
+    while(choice == true)
+    {
+        cout<<"enter"<<endl;
+        cin>>value[i]>>priority[i];
+        ans[priority[i]] = value[i];
+        i++;
+        cnt++;
+        char ch;
+        cin>>ch;
+
+        if(ch =='y' || ch =='Y')
+        {
+            choice = true;
+        }
+        else
+        {
+            choice = false;
+        }
+    }
+    
+    sort(priority,priority+cnt);
+
+    for(int i=cnt-1;i>=0;i--)
+    {
+        cout<<"dequeing element: "<<ans[priority[i]]<<endl;
+    }
+
+
+}
